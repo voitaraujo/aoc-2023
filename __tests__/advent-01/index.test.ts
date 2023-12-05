5739b304efc4b2f01d097f764b90034976c0de41
Voitila Araujo <voitilaaraujo@gmail.com>
2023-12-04 23:42:42 -0300

    advent folder name to variable

--- __tests__/advent-01/index.test.ts
+++ __tests__/advent-01/index.test.ts
@@ -3,29 +3,30 @@ import { describe, expect, test } from "bun:test";
 import { main as part01 } from "@/advents/advent-01/part-01";
 import { main as part02 } from "@/advents/advent-01/part-02";
 
-describe("advent-01 - example", () => {
+const advent_seq = "advent-01";
+
+describe(`${advent_seq} - example`, () => {
   test("part-01", async () => {
-    const result = await part01("./__tests__/advent-01/test-input-01.txt");
+    const result = await part01(`./__tests__/${advent_seq}/test-input-01.txt`);
 
     expect(result).toBe("142");
   });
 
   test("part-02", async () => {
-    const result = await part02("./__tests__/advent-01/test-input-02.txt");
+    const result = await part02(`./__tests__/${advent_seq}/test-input-02.txt`);
     expect(result).toBe("281");
   });
 });
 
-describe("advent-01 - real", () => {
+describe(`${advent_seq} - real`, () => {
   test("part-01", async () => {
-    const result = await part01("./advents/advent-01/input.txt");
+    const result = await part01(`./advents/${advent_seq}/input.txt`);
 
     expect(result).toBe("55447");
   });
 
   test("part-02", async () => {
-    const result = await part02("./advents/advent-01/input.txt");
+    const result = await part02(`./advents/${advent_seq}/input.txt`);
     expect(result).toBe("54706");
   });
 });
-
--- __tests__/advent-02/index.test.ts
+++ __tests__/advent-02/index.test.ts
@@ -3,29 +3,31 @@ import { describe, expect, test } from "bun:test";
 import { main as part01 } from "@/advents/advent-02/part-01";
 import { main as part02 } from "@/advents/advent-02/part-02";
 
-describe("advent-02 - example", () => {
+const advent_seq = "advent-02";
+
+describe(`${advent_seq} - example`, () => {
   test("part-01", async () => {
-    const result = await part01("./__tests__/advent-02/test-input-01.txt");
+    const result = await part01(`./__tests__/${advent_seq}/test-input-01.txt`);
 
     expect(result).toBe(8);
   });
 
   test("part-02", async () => {
-    const result = await part02("./__tests__/advent-02/test-input-02.txt");
+    const result = await part02(`./__tests__/${advent_seq}/test-input-02.txt`);
 
     expect(result).toBe(2286);
   });
 });
 
-describe("advent-02 - real", () => {
+describe(`${advent_seq} - real`, () => {
   test("part-01", async () => {
-    const result = await part01("./advents/advent-02/input.txt");
+    const result = await part01(`./advents/${advent_seq}/input.txt`);
 
     expect(result).toBe(2169);
   });
 
   test("part-02", async () => {
-    const result = await part02("./advents/advent-02/input.txt");
+    const result = await part02(`./advents/${advent_seq}/input.txt`);
 
     expect(result).toBe(60948);
   });
